4cd4403d1915b4385c26de47b9a6d8eabcab0f28
Sergei Melnikov <melnikhov@gmail.com>
2021-04-15 14:49:57 +0300

    Added date and time of request

--- factorio_steam_price_parcer.py
+++ factorio_steam_price_parcer.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
 
@@ -8,13 +9,14 @@ def main():
     soup_id = soup.find(id='game_area_purchase_section_add_to_cart_88199')
     discount_price = soup_id.find(attrs={"class": "discount_final_price"})
     normal_price = soup_id.find(attrs={"class": "game_purchase_price"})
+    current_time = datetime.datetime.now().strftime('%Y.%m.%d %H:%M')
 
     if discount_price is not None:
-        print("Discount price", discount_price.text.strip())
+        print(current_time, "Discount price", discount_price.text.strip())
     elif normal_price is not None:
-        print("Normal price", normal_price.text.strip())
+        print(current_time, "Normal price", normal_price.text.strip())
     else:
-        print("Something went wrong")
+        print(current_time, "Something went wrong")
 
 
 if __name__ == '__main__':
