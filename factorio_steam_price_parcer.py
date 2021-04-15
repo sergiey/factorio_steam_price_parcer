68e56e68655cf304298f5038ad81a77b9cfca7f7
Sergei Melnikov <melnikhov@gmail.com>
2021-04-15 14:35:10 +0300

    Remove test lines

--- factorio_steam_price_parcer.py
+++ factorio_steam_price_parcer.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 
 def main():
     r = requests.get("https://store.steampowered.com/app/427520/Factorio/")
-    # r = requests.get("https://store.steampowered.com/app/1139900/Ghostrunner/")
     soup = BeautifulSoup(r.text)
-    # soup_id = soup.find(id='game_area_purchase_section_add_to_cart_511201')
     soup_id = soup.find(id='game_area_purchase_section_add_to_cart_88199')
     discount_price = soup_id.find(attrs={"class": "discount_final_price"})
     normal_price = soup_id.find(attrs={"class": "game_purchase_price"})
